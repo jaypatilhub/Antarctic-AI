1736c1b463a556b2404c2d6faa4d7457d72231eb
M3 <jay6705patil@gmail.com>
2026-08-25 17:03:22 +0530

    Update data module with Antarctic sample data

--- data_module.py
+++ data_module.py
@@ -1,2 +1,14 @@
+import pandas as pd
+
+
 def load_data():
-    return []
+    data = {
+        "latitude": [-70.5, -71.2],
+        "longitude": [20.3, 21.8],
+        "iceberg_id": ["IB001", "IB002"],
+        "sea_ice_concentration": [85, 72]
+    }
+
+    df = pd.DataFrame(data)
+
+    return df
